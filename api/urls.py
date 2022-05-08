23611099960318fa2ede9b5d432d2d82f6402e4c
pkrfc <mkorolev91@gmail.com>
2022-05-08 16:35:56 +0300

    post-list

--- api/urls.py
+++ api/urls.py
@@ -1,13 +1,14 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from .views import (FollowViewSet, PostViewSet,)
+from .views import (FollowViewSet, PostViewSet, PostsListViewSet)
 
 router = DefaultRouter()
 
 
 router.register('posts', PostViewSet, basename='posts')
 router.register('follow', FollowViewSet, basename='follow')
+router.register('posts-list', PostsListViewSet, basename='posts-list')
 
 
 
--- api/views.py
+++ api/views.py
@@ -1,11 +1,11 @@
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import filters
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
-from rest_framework.viewsets import ModelViewSet
+from rest_framework.viewsets import ModelViewSet, ReadOnlyModelViewSet
 
 from posts.models import Follow, Post
 
-from .serializers import FollowSerializer, PostSerializer
+from .serializers import FollowSerializer, PostSerializer, PostListSerializer
 
 
 class PostViewSet(ModelViewSet):
@@ -32,3 +32,17 @@ class FollowViewSet(ModelViewSet):
 
     def perform_destroy(self, instance):
         instance.delete()
+
+
+class PostsListViewSet(ReadOnlyModelViewSet):
+    """Post list"""
+    serializer_class = PostListSerializer()
+    permission_classes = [IsAuthenticatedOrReadOnly]
+    filter_backends = (DjangoFilterBackend, filters.OrderingFilter)
+    ordering_fields = ('pub_date',)
+
+    def get_queryset(self):
+        followed_people = Follow.objects.filter(
+            user=self.request.user).values('following'
+                                           )
+        return Post.objects.filter(author__in=followed_people)
