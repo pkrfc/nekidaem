ec9b23b5627c5cad59de838a4cf9d83b4d8379e3
pkrfc <mkorolev91@gmail.com>
2022-05-08 14:55:20 +0300

    models v. 1.0 posts/follow

---
+++ posts/models.py
@@ -0,0 +1,32 @@
+from django.contrib.auth import get_user_model
+from django.db import models
+
+User = get_user_model()
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=140)
+    text = models.TextField()
+    pub_date = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='posts',
+        blank=True,
+    )
+
+    def __str__(self):
+        return self.text
+
+
+class Follow(models.Model):
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='followers',
+    )
+    following = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='following'
+    )
