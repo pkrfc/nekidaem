e0e5a33bb41defce34e622bf48ae0aa10d516c00
pkrfc <mkorolev91@gmail.com>
2022-05-08 15:45:14 +0300

    v 1.0 Post/Follow

--- api/views.py
+++ api/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import filters
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from posts.models import Follow, Post
+
+from .serializers import FollowSerializer, PostSerializer
+
+
+class PostViewSet(ModelViewSet):
+    """Post create"""
+    queryset = Post.objects.all()
+    permission_classes = [IsAuthenticatedOrReadOnly]
+    serializer_class = PostSerializer
+    filter_backends = (DjangoFilterBackend, filters.OrderingFilter)
+    ordering_fields = ('pub_date',)
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+
+class FollowViewSet(ModelViewSet):
+    """Follow"""
+    serializer_class = FollowSerializer
+
+    def get_queryset(self):
+        return Follow.objects.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+    def perform_destroy(self, instance):
+        instance.delete()
