60e6a85a0e64c592b37249cc3e3fcbb51f766256
pkrfc <mkorolev91@gmail.com>
2022-05-08 15:45:53 +0300

    v 1.0 urls

---
+++ api/urls.py
@@ -0,0 +1,16 @@
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
+
+from .views import (FollowViewSet, PostViewSet,)
+
+router = DefaultRouter()
+
+
+router.register('posts', PostViewSet, basename='posts')
+router.register('follow', FollowViewSet, basename='follow')
+
+
+
+urlpatterns = [
+    path('v1/', include(router.urls)),
+]
