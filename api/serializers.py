ce5b0aa26f3299be45b8b02daf638c5662fa08b7
pkrfc <mkorolev91@gmail.com>
2022-05-08 15:45:31 +0300

    v 1.0 serializers

---
+++ api/serializers.py
@@ -0,0 +1,51 @@
+from rest_framework import serializers
+from rest_framework.relations import SlugRelatedField
+
+
+from posts.models import Follow, Post, User
+
+
+class PostSerializer(serializers.ModelSerializer):
+    author = SlugRelatedField(
+        slug_field='username', read_only=True
+    )
+    views_user = SlugRelatedField(
+        slug_field='username', read_only=True
+    )
+
+    class Meta:
+        fields = '__all__'
+        model = Post
+
+
+class FollowSerializer(serializers.ModelSerializer):
+    user = SlugRelatedField(
+        slug_field='username',
+        read_only=True,
+        default=serializers.CurrentUserDefault()
+    )
+    following = serializers.SlugRelatedField(
+        slug_field='username',
+        queryset=User.objects.all()
+    )
+
+    def validate_following(self, following):
+
+        user = self.context['request'].user
+
+        if user != following:
+
+            return following
+
+        raise serializers.ValidationError('cant subscribe')
+
+    class Meta:
+        fields = '__all__'
+        model = Follow
+        validators = [
+            serializers.UniqueTogetherValidator(
+                queryset=Follow.objects.all(),
+                fields=('user', 'following')
+            ),
+        ]
+
